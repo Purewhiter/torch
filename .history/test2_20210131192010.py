645d8aa55a8b983753412f05f24d91630bac971a
Purewhiter <Purewhiter@outlook.com>
2021-01-31 19:22:53 +0800

    torch

---
+++ .history/test2_20210131192010.py
@@ -0,0 +1,7 @@
+'''
+Author      : PureWhite
+Date        : 2021-01-31 19:20:08
+LastEditors : PureWhite
+LastEditTime: 2021-01-31 19:20:09
+Description : 
+'''
---
+++ .history/test2_20210131192018.py
@@ -0,0 +1,8 @@
+'''
+Author      : PureWhite
+Date        : 2021-01-31 19:20:08
+LastEditors : PureWhite
+LastEditTime: 2021-01-31 19:20:10
+Description : 
+'''
+print("ccn")
