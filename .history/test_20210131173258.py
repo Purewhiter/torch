7fc5d4eef8e7793ba6ceb96685d0291f834c500c
Purewhiter <Purewhiter@outlook.com>
2021-01-31 19:19:54 +0800

    torch

---
+++ .history/test_20210131173258.py
@@ -0,0 +1,7 @@
+'''
+Author      : PureWhite
+Date        : 2021-01-31 17:32:55
+LastEditors : PureWhite
+LastEditTime: 2021-01-31 17:32:58
+Description : 
+'''
---
+++ test.py
@@ -0,0 +1,8 @@
+'''
+Author      : PureWhite
+Date        : 2021-01-31 17:32:55
+LastEditors : PureWhite
+LastEditTime: 2021-01-31 17:33:22
+Description : 
+'''
+print("hhh");
